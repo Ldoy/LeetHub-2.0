78a0e490c36c667a58a359fb1926906e16c7093f
Do Yi Lee <34529917+Ldoy@users.noreply.github.com>
2024-06-20 10:30:42 +0900

    Time: 22 ms (65.65%), Space: 16.7 MB (41.79%) - LeetHub

--- LeetCode/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.swift
+++ LeetCode/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.swift
@@ -1,22 +1,22 @@
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
         //nums 은 오름차순, 
-        // var tempSet = Set(nums)
-        // nums = Array(tempSet.sorted())
-        // return tempSet.count
+        var tempSet = Set(nums)
+        nums = Array(tempSet.sorted())
+        return tempSet.count
         // 중복되는 유닉 엘리먼트를 찾아야함. 남은 원소 수를 반환
 
-        //Solution2 - for 문 돌면서 동일하지 않은 것들만 임시 배열에 담기
-        var tempArray = [Int]()
-        for num in nums {
-            if tempArray.contains(num) {
-                continue
-            } else {
-                tempArray.append(num)
-            }
-        }
+        //Solution2 - for 문 돌면서 동일하지 않은 것들만 임시 배열에 담기, 27ms
+        // var tempArray = [Int]()
+        // for num in nums {
+        //     if tempArray.contains(num) {
+        //         continue
+        //     } else {
+        //         tempArray.append(num)
+        //     }
+        // }
     
-        nums = tempArray.sorted()
+        // nums = tempArray.sorted()
         return nums.count
         //Solution3 - 고차함수 이용하기
         
