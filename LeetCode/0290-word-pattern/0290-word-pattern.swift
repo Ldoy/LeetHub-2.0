0165e004deb8f1a0693073a6e3939846e9e538ff
Do Yi Lee <34529917+Ldoy@users.noreply.github.com>
2024-07-24 14:47:47 +0900

    Time: 4 ms (42.93%), Space: 16.4 MB (76.44%) - LeetHub

---
+++ LeetCode/0290-word-pattern/0290-word-pattern.swift
@@ -0,0 +1,36 @@
+class Solution {
+func wordPattern(_ pattern: String, _ s: String) -> Bool {
+    //pattern -s
+    var patternMap = [Character: String]()
+    
+    //s - pattern
+    var sMap = [String: Character]()
+    
+    var sArray = s.split(separator: " ").map { String($0) }
+    var pArray = pattern.map { Character(extendedGraphemeClusterLiteral: $0) }
+    
+   
+    
+    if sArray.count != pArray.count {
+        return false
+    }
+
+    for i in 0..<pArray.count {
+        let tempP = pArray[i]
+
+        let tempS = sArray[i]
+        
+        if (patternMap[tempP] != nil && patternMap[tempP] != tempS) || (sMap[tempS] != nil && sMap[tempS] != tempP) {
+            return false
+        }
+        
+        patternMap[tempP] = tempS
+        sMap[tempS] = tempP
+        
+        // 서로 다른게 매칭되어있는 경우 false
+    }
+    
+    return true
+}
+
+}
