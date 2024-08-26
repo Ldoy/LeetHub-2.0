12c9b000135b9047d14a2867c11af7dec128c06f
Do Yi Lee <34529917+Ldoy@users.noreply.github.com>
2024-08-26 20:02:54 +0900

    Time: 27 ms (19.48%), Space: 16.4 MB (76.72%) - LeetHub

--- LeetCode/0242-valid-anagram/0242-valid-anagram.swift
+++ LeetCode/0242-valid-anagram/0242-valid-anagram.swift
@@ -1,9 +1,38 @@
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
-        if s.sorted() == t.sorted() {
-            return true
-        } else {
-            return false
-        }
-    }
+      if s.count != t.count {
+          return false
+      }
+
+      var sHash = [String: Int]()
+      var tHash = [String: Int]()
+      s.map { cha in
+          if (sHash[String(cha)] != nil) {
+              sHash[String(cha)]! += 1
+          } else {
+              sHash[String(cha)] = 1
+          }
+      }
+
+      t.map { cha in
+          if (tHash[String(cha)] != nil) {
+              tHash[String(cha)]! += 1
+          } else {
+              tHash[String(cha)] = 1
+          }
+      }
+
+      for (key, value) in sHash {
+          if tHash[key] == value {
+              continue
+          } else {
+              return false
+          }
+      }
+
+      return true
+  }
+
+
+
 }
