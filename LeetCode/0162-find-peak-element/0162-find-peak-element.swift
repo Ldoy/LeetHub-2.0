36117adf10101e50cde9a56a4319e6864df19850
Do Yi Lee <34529917+Ldoy@users.noreply.github.com>
2024-09-02 11:04:09 +0900

    Time: 12 ms (35.98%), Space: 15.3 MB (90.24%) - LeetHub

---
+++ LeetCode/0162-find-peak-element/0162-find-peak-element.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func findPeakElement(_ nums: [Int]) -> Int {
+        //mid -> mid-1, mid + 1
+        //if  -> mid-1 < mid // 
+                // mid > mid + 1 => return 
+                // mid < mid + 1 => mid는 왼쪽으로 
+        // if -> mid-1 > mid // 4,1,n
+                // mid > mid+1 =>  4,3,1 mid 왼쪽으로 
+                // mid < mid+1 => 4,3,5 mid 왼쪽 오른쪽 모두 check
+                // => Go any way depending on which side your program is checking. Both side there is at least 1 peak
+        var left = 0
+        var right = nums.count - 1
+
+        while left <= right {
+            let mid = (left + right) / 2
+
+            if mid > 0, nums[mid] < nums[mid - 1] {
+                right = mid - 1
+            } else if mid < nums.count - 1, nums[mid] < nums[mid + 1] {
+                left = mid + 1
+            } else {
+                return mid
+            }
+        }
+        return right
+    }       
+
+    func check(nums: [Int], numIndex: Int) -> Int? {
+        if nums[numIndex] > nums[numIndex - 1], nums[numIndex] > nums[numIndex + 1] {
+            return numIndex
+        } else {
+            return nil
+        }
+    }
+}
