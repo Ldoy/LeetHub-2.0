d1635fa0dbfee37f99f10566960a635de35a9a33
Do Yi Lee <34529917+Ldoy@users.noreply.github.com>
2024-09-18 11:10:41 +0900

    Time: 14 ms (84.04%), Space: 16 MB (66.78%) - LeetHub

--- LeetCode/0110-balanced-binary-tree/0110-balanced-binary-tree.swift
+++ LeetCode/0110-balanced-binary-tree/0110-balanced-binary-tree.swift
@@ -14,38 +14,21 @@
  * }
  */
 class Solution {
-    var left = 0
-    var right = 0
-var flag = true
-
    func isBalanced(_ root: TreeNode?) -> Bool {
-     //양쪽 노드비교 => 누적 => 2이상 차이나면 false
+    func check(_ root: TreeNode?) -> (Bool, Int) {
 
-      func check(_ root: TreeNode?) -> Int {
-         if root == nil {
-             return 0
-         } else {
-             let left = check(root?.left)
-             let right = check(root?.right)
-             
-             if abs(left - right) >= 2  {
-                 flag = false
-             }
-             
-             return max(left, right) + 1
-         }
-     }
-
-     let left = check(root?.left)
-     let right = check(root?.right)
-    
-        if flag == false {
-            return false
+            if root == nil {
+                return (true, 0)
+            } else {
+                let left = check(root?.left)
+                let right = check(root?.right)
+                
+                let balanced = abs(left.1 - right.1) <= 1 && left.0 && right.0
+                
+                return (balanced, max(left.1, right.1) + 1)
+            }
         }
-     if abs(left - right) >= 2  {
-         return false
-     } else {
-         return true
-     }
+        
+    return check(root).0
  }
 }
