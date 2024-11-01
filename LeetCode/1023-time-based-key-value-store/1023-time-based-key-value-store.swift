6f52c9da4711cedd797ab1c59b9eabedff2cdbe0
Do Yi Lee <34529917+Ldoy@users.noreply.github.com>
2024-11-01 11:23:36 +0900

    Time: 52 ms (30.77%), Space: 32.9 MB (56.1%) - LeetHub

--- LeetCode/1023-time-based-key-value-store/1023-time-based-key-value-store.swift
+++ LeetCode/1023-time-based-key-value-store/1023-time-based-key-value-store.swift
@@ -1,41 +1,47 @@
+
 class TimeMap {
     private var hash: [String: [(Int, String)]]
-
     init() {
-        self.hash = [String: [(Int, String)]]()
+        self.hash = [String: [(key: Int, value: String)]]()
     }
-
+    
     func set(_ key: String, _ value: String, _ timestamp: Int) {
-        // 키가 존재하지 않으면 새로운 리스트 생성
         if hash[key] == nil {
             hash[key] = []
         }
-        // 해당 키에 (타임스탬프, 값) 쌍을 추가
         hash[key]!.append((timestamp, value))
+        
     }
-
+    
     func get(_ key: String, _ timestamp: Int) -> String {
-        guard let timeValues = hash[key] else {
+        guard let pair = hash[key] else {
             return ""
         }
 
-        // 이진 검색으로 가장 가까운 타임스탬프 찾기
         var left = 0
-        var right = timeValues.count - 1
+        var right = pair.count - 1
         var result = ""
-
         while left <= right {
-            let mid = (left + right) / 2
-            if timeValues[mid].0 == timestamp {
-                return timeValues[mid].1
-            } else if timeValues[mid].0 < timestamp {
-                result = timeValues[mid].1
+            let mid = (left + right)/2
+            
+            if pair[mid].0 == timestamp {
+                return pair[mid].1
+            } else if pair[mid].0 < timestamp {
+                result = pair[mid].1
                 left = mid + 1
             } else {
-                right = mid - 1
+                right = mid - 1 
             }
+
         }
 
         return result
     }
 }
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * let obj = TimeMap()
+ * obj.set(key, value, timestamp)
+ * let ret_2: String = obj.get(key, timestamp)
+ */
